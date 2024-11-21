5610e7490d1bc9f6cce3261f403c508a98179013
Alexis M. Daugé <69150061+mitsuakki@users.noreply.github.com>
2024-11-21 12:53:45 +0000

    submit: Runtime - 743 ms (67.81%), Memory - 0.0B (100.00%)

---
+++ submissions/1390-average-selling-price/solution.sql
@@ -0,0 +1,5 @@
+SELECT p.product_id, IFNULL(ROUND(SUM(p.price * u.units) / SUM(u.units), 2), 0) AS average_price
+FROM Prices p
+LEFT JOIN UnitsSold AS u ON p.product_id = u.product_id 
+WHERE (u.purchase_date BETWEEN p.start_date AND p.end_date) OR u.purchase_date IS NULL
+GROUP BY p.product_id
