bf4c9602ca7ea980fe00047505ade6c40c6392c6
Alexis M. Daugé <69150061+mitsuakki@users.noreply.github.com>
2024-12-02 10:26:28 +0000

    submit: Runtime - 1233 ms (10.89%), Memory - 0.0B (100.00%)

---
+++ submissions/1773-percentage-of-users-attended-a-contest/solution.sql
@@ -0,0 +1,4 @@
+SELECT r.contest_id, ROUND(COUNT(r.user_id) * 100.0 / (SELECT COUNT(u.user_id) FROM Users u), 2) AS percentage
+FROM Register r
+GROUP BY r.contest_id
+ORDER BY percentage DESC, r.contest_id ASC;
